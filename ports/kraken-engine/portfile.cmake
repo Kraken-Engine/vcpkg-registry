4c734d131955e91e96fdfc042cbff49e06b9fe63
Derrick Martinez <103585220+durkisneer1@users.noreply.github.com>
2026-04-15 10:03:47 -0500

    Init registry and add v1.7.1

---
+++ ports/kraken-engine/portfile.cmake
@@ -0,0 +1,17 @@
+vcpkg_from_github(
+    REPO Kraken-Engine/PyKraken
+    REF v1.7.1
+    SHA512 308861017466c4778085a0feae97988a093916f6265b8afbd846e09e4e72e2e87649b67f3990a2eff259c6e2e486b93710a7e5c650c6809eeae4b9274424875c
+    HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DKRAKEN_BUILD_PYTHON=OFF
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME KrakenEngine CONFIG_PATH share/kraken-engine)
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
